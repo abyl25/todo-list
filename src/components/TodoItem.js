16c26e92232b5bd2b154220ce0b849b122366808
Абылай Тастанбеков <abylay.tastanbekov@nu.edu.kz>
2019-02-04 14:02:58 +0600

    connected backend api

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,11 +4,11 @@ import '../App.css';
 class TodoItem extends Component {
 
     render() {
-        const { id, title } = this.props.item;
+        const { _id, title } = this.props.item;
         return (
             <p className="item">{title} 
-                <button className="edit-btn" onClick={this.props.editItem.bind(this, id)}>Edit</button>
-                <button className="delete-btn" onClick={this.props.deleteItem.bind(this, id)}>Delete</button> 
+                {/* <button className="edit-btn" onClick={this.props.editItem.bind(this, _id)}>Edit</button> */}
+                <button className="delete-btn" onClick={this.props.deleteItemAction.bind(this, _id)}>Delete</button> 
             </p>          
         );
     }
